c74bc8cf26746ccc99de7d6fdb570097f1d9db5d
ercan1102 <saidgasanov8@gmail.com>
2021-10-22 19:36:11 +0000

    Update qemu.bat, mac.bat, img to vhd.bat

--- img to vhd.bat
+++ img to vhd.bat
@@ -1,4 +1,3 @@
-@echo off
 :: Converting from IMG disk to VHD (only hard disk)
 cd C:\Program Files\Oracle\VirtualBox
 vboxmanage convertdd C:\c.img C:\c.vhd
--- mac.bat
+++ mac.bat
@@ -1,4 +1,3 @@
-@echo off
 :: This allows running MacOS in VirtualBox
 cd "C:\Program Files\Oracle\VirtualBox\"
 VBoxManage modifyvm "mac" --cpuidset 00000001 000306a9 00020800 80000201 178bfbff
