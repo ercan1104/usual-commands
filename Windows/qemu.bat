1c171e1cbc8ec11a8ad7b8da3146a34aeac916d2
ercan1102 <saidgasanov8@gmail.com>
2023-03-06 22:46:36 +0300

    Update QEMU start script

--- Windows/qemu.bat
+++ Windows/qemu.bat
@@ -1,4 +1,4 @@
 :: Script for running QEMU x64
 :: For program to start working, you have to move your image to C:\
 cd "C:\Program Files\qemu"
-qemu-system-x86_64w.exe -m 512 -vga cirrus -device rtl8139,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -hda "C:\c.img"
+qemu-system-x86_64w.exe -m 512 -vga cirrus -device rtl8139,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -drive file="C:\c.img",format=raw,index=0,media=disk
