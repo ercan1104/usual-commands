d7b3f7325f3a221c335e960b72db2e3d4b262873
ercan1104 <saidgasanov8@gmail.com>
2024-03-15 23:01:09 +0300

    Bugfixes and bug improvements

--- Linux/internet.sh
+++ Linux/internet.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ip link set eth0 up
 dhcpcd
+
+if [ $? -eq 127 ]; then
+    dhclient eth0
+fi
--- Linux/rw_ufs_filesystem.sh
+++ Linux/rw_ufs_filesystem.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo mount -t ufs -o ufstype=ufs2
+sudo mount -t ufs -o ufstype=ufs2 /dev/sdX /mnt
