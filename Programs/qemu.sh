35a106df5c345dbe41e058767d99a0acb4ed6f48
ercan1104 <ercan1104@protonmail.com>
2025-08-09 16:50:27 +0300

    QEMU script example

--- Programs/qemu.sh
+++ Programs/qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 -m 512 -vga cirrus -device rtl8139,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -drive file=c.img,format=raw,index=0,media=disk
+qemu-system-x86_64 -net bridge,br=virbr0 -net nic,model=virtio -drive file=disk.img,format=raw,index=0,media=disk
